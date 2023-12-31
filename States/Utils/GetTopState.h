25a8599814244d55d54d02fc71ab4b614ffe9c70
brunomgferreira <brunomgferreira17@gmail.com>
2023-12-31 03:17:44 +0000

    GetTopState documentation is done

--- States/Utils/GetTopState.h
+++ States/Utils/GetTopState.h
@@ -4,13 +4,50 @@
 #include <functional>
 #include "States/State.h"
 
+/**
+* @brief Class that represents a state for obtaining the number of stops with a upper limit as user input.
+*/
+
 class GetTopState : public State {
 private:
     State* backState;
     function<void(App*, int)> nextStateCallback;
 public:
+
+    /**
+    * @brief Constructs an instance of GetTopState with specified back state and callback function.
+    *
+    * @details This constructor initializes an instance of the GetTopState class with the given back state and
+    * a callback function for transitioning to the next state. The back state represents the state to which the
+    * application should return when the user chooses to go back from the current state. The callback function
+    * specifies the action to be performed in the next state, usually involving capturing a top number from user
+    * input and proceeding to the next state with the captured information.
+    *
+    * @param backState A pointer to the state to which the application should return when the user chooses to go back.
+    * @param nextStateCallback A function defining the action to be performed in the next state, usually involving a top number.
+    */
     GetTopState(State* backState, function<void(App*, int)> nextStateCallback);
+
+    /**
+    * @brief Displays a prompt for inserting the number of top elements.
+    *
+    * @details This method prints a prompt to the console, asking the user to insert the number of top elements.
+    * The displayed prompt guides the user on the expected format for entering the number of top elements. The user is
+    * expected to input a valid integer representing the desired number of top elements for a specific operation.
+    */
     void display() const override;
+
+    /**
+    * @brief Handles user input for obtaining the number of top elements.
+    *
+    * @details This method reads a line of input from the console, representing the desired number of top elements for a specific operation.
+    * The method validates the entered input, ensuring it is a valid positive integer not greater than 3019 (the maximum number of airports).
+    * If the input is valid, the callback function is invoked with the captured number of top elements, and the application transitions to the next
+    * state accordingly. If the input is invalid, the user is prompted with an error message, and the state transitions to a "Try Again" state,
+    * allowing the user to make another attempt.
+    *
+    * @param app A pointer to the application instance.
+    */
     void handleInput(App* app) override;
 };
 
