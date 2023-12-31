005889900dc1cd0d3c1301e90158c8db767e5a2a
brunomgferreira <brunomgferreira17@gmail.com>
2023-12-31 15:57:07 +0000

    Removed unused methods from Airline class

--- Airline.cpp
+++ Airline.cpp
@@ -6,35 +6,3 @@ Airline::Airline(string code, string name, string callsign, string country) {
     this->callsign = callsign;
     this->country = country;
 }
-
-const string &Airline::getCode() const {
-    return code;
-}
-
-void Airline::setCode(const string &code) {
-    Airline::code = code;
-}
-
-const string &Airline::getName() const {
-    return name;
-}
-
-void Airline::setName(const string &name) {
-    Airline::name = name;
-}
-
-const string &Airline::getCallsign() const {
-    return callsign;
-}
-
-void Airline::setCallsign(const string &callsign) {
-    Airline::callsign = callsign;
-}
-
-const string &Airline::getCountry() const {
-    return country;
-}
-
-void Airline::setCountry(const string &country) {
-    Airline::country = country;
-}
