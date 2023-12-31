d4cfbd7ce45f8fc5ed7428fd7e3f339d677d127f
brunomgferreira <brunomgferreira17@gmail.com>
2023-12-31 15:58:59 +0000

    Removed unused methods from City class

--- City.cpp
+++ City.cpp
@@ -5,30 +5,10 @@ City::City(const string &name, const string &countryName, const string &airportC
     airports.insert(airportCode);
 }
 
-const string &City::getName() const {
-    return name;
-}
-
-void City::setName(const string &name) {
-    this->name = name;
-}
-
-const string &City::getCountryName() const {
-    return countryName;
-}
-
-void City::setCountryName(const string &countryName) {
-    this->countryName = countryName;
-}
-
 const unordered_set<string> &City::getAirports() const {
     return airports;
 }
 
 void City::addAirport(const string &airportCode) {
     airports.insert(airportCode);
-}
-
-void City::setAirports(const unordered_set<string> &airports) {
-    this->airports = airports;
 }
